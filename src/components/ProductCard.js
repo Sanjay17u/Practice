c0942019f73e15b707898c7c89156d5326ba9b54
Sanjay Solanki <sanjay.solanki.17.om@gmail.com>
2024-12-06 20:51:04 +0530

    Practicing UseState

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,19 +1,30 @@
-import React from "react";
-import { productList } from '../utils/constant'; 
+import React, { useState } from "react";
+import { productList } from "../utils/constant";
 import Product from "./Product";
 
 const ProductCard = () => {
+  // Local State Variable
   console.log("productList:", productList);
+  const [listOfProduct, setListOfProduct] = useState(productList)
+
 
   return (
-    <div className="product_card">
-      {productList.length === 0 ? (
-        <p>No products available</p>
-      ) : (
-        productList.map((product) => (
-          <Product key={product.id} product={product} /> 
-        ))
-      )}
+    <div>
+      <button onClick={() => {
+
+        const FilteredProduct = listOfProduct.filter(product => product.rating.rate >= 4)
+        setListOfProduct(FilteredProduct)
+
+      }} className={{"margin-top": "10px"}}>Top Rated product</button>
+      <div className="product_card">
+        {productList.length === 0 ? (
+          <p>No products available</p>
+        ) : (
+          listOfProduct.map((product) => (
+            <Product key={product.id} product={product} />
+          ))
+        )}
+      </div>
     </div>
   );
 };
