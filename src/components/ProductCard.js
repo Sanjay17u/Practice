e6e188cdef56e052d1f5be250696e0a60164077a
Sanjay Solanki <sanjay.solanki.17.om@gmail.com>
2024-12-08 18:27:40 +0530

    add Filter Functionality.

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { productList } from "../utils/constant";
 import Product from "./Product";
+import Skeleton from "./Skeleton";
 
 const ProductCard = () => {
   // Local State Variable
   console.log("productList:", productList);
   const [listOfProduct, setListOfProduct] = useState([])
+  const [filteredProduct, setFilteredProduct] = useState([])
+  const [searchText, setSearchText] = useState("")
 
   useEffect(() => {
     fetchData()
@@ -16,21 +19,37 @@ const ProductCard = () => {
     const resData = await data.json();
     console.log(resData)
     setListOfProduct(resData)
+    setFilteredProduct(resData)
   } 
 
-  return (
+
+  // if(listOfProduct.length === 0) {
+  //   return <Skeleton/>
+  // }
+
+
+  return listOfProduct.length === 0 ? <Skeleton/> : (
     <div>
+      <div style={{"margin-top": "10px"}}>
+        <input type="text" onChange={(e) => setSearchText(e.target.value)}  value={searchText}/>
+        <button onClick={() => {
+          const filteredData = listOfProduct.filter((product) => {
+            return product.title.toLowerCase().includes(searchText.toLowerCase())
+          })
+          setFilteredProduct(filteredData)
+        }}>Search</button>
+      </div>
       <button onClick={() => {
 
         const FilteredProduct = listOfProduct.filter(product => product.rating.rate >= 4)
         setListOfProduct(FilteredProduct)
 
-      }} className={{"margin-top": "10px"}}>Top Rated product</button>
+      }} style={{"margin-top": "10px"}}>Top Rated product</button>
       <div className="product_card">
         {productList.length === 0 ? (
           <p>No products available</p>
         ) : (
-          listOfProduct.map((product) => (
+          filteredProduct.map((product) => (
             <Product key={product.id} product={product} />
           ))
         )}
---
+++ src/components/Skeleton.js
@@ -0,0 +1,21 @@
+const Skeleton = () => {
+  return (
+    <>
+    <div className="skeleton_container">
+      <div className="skeleton"></div>
+      <div className="skeleton"></div>
+      <div className="skeleton"></div>
+      <div className="skeleton"></div>
+      <div className="skeleton"></div>
+      <div className="skeleton"></div>
+      <div className="skeleton"></div>
+      <div className="skeleton"></div>
+      <div className="skeleton"></div>
+      <div className="skeleton"></div>
+      <div className="skeleton"></div>
+    </div>
+    </>
+  );
+};
+
+export default Skeleton
